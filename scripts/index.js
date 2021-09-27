f8e3338367e8e3aead24ed196d2f0e3bd3bb2145
Evgeniy Sergeev <akaevgeniy@yandex.ru>
2021-09-27 17:52:56 +0300

    Feature: добавлен скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,25 @@
 const editForm = document.querySelector(".popup");
 const editFormClose = editForm.querySelector(".popup__close");
-const editButtonClc = document.querySelector(".profile__edit-button");
+const editButtonActive = document.querySelector(".profile__edit-button");
+const formPopup = editForm.querySelector(".popup__container");
+let nameInput = formPopup.querySelector(".popup__name");
+let aboutInput = formPopup.querySelector(".popup__about");
+let namePage = document.querySelector(".profile__name");
+let aboutPage = document.querySelector(".profile__about");
 
 function toggleEditForm() {
   editForm.classList.toggle("popup_is-opened");
 }
 
-editButtonClc.addEventListener("click", toggleEditForm);
+nameInput.value = namePage.textContent;
+aboutInput.value = aboutPage.textContent;
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  namePage.textContent = nameInput.value;
+  aboutPage.textContent = aboutInput.value;
+  toggleEditForm();
+}
+editButtonActive.addEventListener("click", toggleEditForm);
 editFormClose.addEventListener("click", toggleEditForm);
+formPopup.addEventListener("submit", formSubmitHandler);
