d16a45b17e14222af9e28eb3109115a636f14bd4
Evgeniy Sergeev <akaevgeniy@yandex.ru>
2021-09-28 14:59:55 +0300

    Feature: отлажен скрипт, добавлен ридми файл

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,24 @@
-const editForm = document.querySelector(".popup");
-const editFormClose = editForm.querySelector(".popup__close");
-const editButtonActive = document.querySelector(".profile__edit-button");
-const formPopup = editForm.querySelector(".popup__container");
-let nameInput = formPopup.querySelector(".popup__name");
-let aboutInput = formPopup.querySelector(".popup__about");
-let namePage = document.querySelector(".profile__name");
-let aboutPage = document.querySelector(".profile__about");
-nameInput.value = namePage.textContent;
-aboutInput.value = aboutPage.textContent;
+const editForm = document.querySelector('.popup');
+const editFormClose = editForm.querySelector('.popup__close');
+const editButtonActive = document.querySelector('.profile__edit-button');
+const formPopup = editForm.querySelector('.popup__container');
+let nameInput = formPopup.querySelector('.popup__name');
+let aboutInput = formPopup.querySelector('.popup__about');
+let namePage = document.querySelector('.profile__name');
+let aboutPage = document.querySelector('.profile__about');
 
 function toggleEditForm() {
-  editForm.classList.toggle("popup_is-opened");
+  editForm.classList.toggle('popup_is-opened');
+  nameInput.value = namePage.textContent;
+  aboutInput.value = aboutPage.textContent;
 }
-editButtonActive.addEventListener("click", toggleEditForm);
-editFormClose.addEventListener("click", toggleEditForm);
+editButtonActive.addEventListener('click', toggleEditForm);
+editFormClose.addEventListener('click', toggleEditForm);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   namePage.textContent = nameInput.value;
   aboutPage.textContent = aboutInput.value;
-  //   toggleEditForm();
+  editForm.classList.remove('popup_is-opened');
 }
-formPopup.addEventListener("submit", formSubmitHandler);
+formPopup.addEventListener('submit', formSubmitHandler);
