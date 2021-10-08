e84b10580a7422a85d87514c9e1dbef2985af2a6
Evgeniy Sergeev <akaevgeniy@yandex.ru>
2021-10-08 14:48:20 +0300

    Feat: Script add

--- scripts/index.js
+++ scripts/index.js
@@ -2,21 +2,27 @@
 const layerPopup = document.querySelector('.popup');
 const popupCloseButton = layerPopup.querySelector('.popup__close');
 const editButtonActive = document.querySelector('.profile__edit-button');
-const formPopup = layerPopup.querySelector('.popup__form');
-let nameInput = formPopup.querySelector('.popup__input_is_name');
-let aboutInput = formPopup.querySelector('.popup__input_is_about');
+const addButtonActive = document.querySelector('.profile__add-button');
+const formPopups = document.querySelectorAll('.popup__form');
+let nameInput = formPopups[0].querySelector('.popup__input_is_name');
+let aboutInput = formPopups[0].querySelector('.popup__input_is_about');
+let nameAddInput = formPopups[1].querySelector('.popup__input_is_add-name');
+let linkAddInput = formPopups[1].querySelector('.popup__input_is_add-link');
 let namePage = document.querySelector('.profile__name');
 let aboutPage = document.querySelector('.profile__about');
 // Функция для открытия модального окна, добавляем попапу класс,
 // при открытии окна данные со страницы записываюся в инпуты формы
-function addPopup() {
+function addPopup(index) {
   layerPopup.classList.add('popup_is-opened');
+  formPopups[index].classList.add('popup__form_is-opened');
   nameInput.value = namePage.textContent;
   aboutInput.value = aboutPage.textContent;
 }
 // Функция для закрытия модального окна, удаляем у попапа класс
 function removePopup() {
   layerPopup.classList.remove('popup_is-opened');
+  formPopups[0].classList.remove('popup__form_is-opened');
+  formPopups[1].classList.remove('popup__form_is-opened');
 }
 // Переопределяем submit для перезаписывания полей из инпутов на страницу
 function formSubmitHandler(evt) {
@@ -26,6 +32,7 @@ function formSubmitHandler(evt) {
   removePopup();
 }
 // Вешаем слушатели событий для открытия/закрытия попапа и пересохранения данных
-editButtonActive.addEventListener('click', addPopup);
+editButtonActive.addEventListener('click', () => addPopup(0));
+addButtonActive.addEventListener('click', () => addPopup(1));
 popupCloseButton.addEventListener('click', removePopup);
-formPopup.addEventListener('submit', formSubmitHandler);
+formPopups[0].addEventListener('submit', formSubmitHandler);
