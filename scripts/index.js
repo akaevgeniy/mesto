5acb03219b0b2aa4e90291027656c0f4e0d82bdd
Evgeniy Sergeev <akaevgeniy@yandex.ru>
2021-10-08 18:22:35 +0300

    Feat: Script template add

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,32 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
 // Находим в DOM попап, форму, инпуты и кнопки редактирования, закрытия формы
+const elementContainer = document.querySelector('.elements');
+const elementTemplate = document.querySelector('.element-template').content;
 const layerPopup = document.querySelector('.popup');
 const popupCloseButton = layerPopup.querySelector('.popup__close');
 const editButtonActive = document.querySelector('.profile__edit-button');
@@ -10,6 +38,18 @@ let nameAddInput = formPopups[1].querySelector('.popup__input_is_add-name');
 let linkAddInput = formPopups[1].querySelector('.popup__input_is_add-link');
 let namePage = document.querySelector('.profile__name');
 let aboutPage = document.querySelector('.profile__about');
+
+const renderElement = (taskName) => {
+  const sectionElement = elementTemplate.cloneNode(true);
+  const sectionElementTitle = sectionElement.querySelector('.element__title');
+  const sectionElementPhoto = sectionElement.querySelector('.element__photo');
+  sectionElementPhoto.src = taskName.link;
+  sectionElementPhoto.alt = taskName.name;
+  sectionElementTitle.textContent = taskName.name;
+  elementContainer.append(sectionElement);
+};
+
+initialCards.forEach(renderElement);
 // Функция для открытия модального окна, добавляем попапу класс,
 // при открытии окна данные со страницы записываюся в инпуты формы
 function addPopup(index) {
